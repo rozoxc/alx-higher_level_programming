1c28f1d7305aafb4271106095f8c1f3ec7c2a78a
he <abdel.brave@gmail.com>
2023-11-28 14:51:52 -0800

    sl

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number < 10:
-        print("0". format(number), end="")
-    print(number)
+for num in range(100):
+    print("{:02d}".format(num), end=", " if num < 99 else "\n")
