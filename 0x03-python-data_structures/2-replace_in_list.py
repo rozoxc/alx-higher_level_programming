0bab4b193412c7f1f61cd475415f0dde6f587807
he <abdel.brave@gmail.com>
2023-12-03 08:14:22 -0800

    ld

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if (idx < 0):
+        return None
+    for i in range(0, len(my_list)):
+        if (i == idx):
+            my_list[i] = element
+            return(my_list)
+    return None
