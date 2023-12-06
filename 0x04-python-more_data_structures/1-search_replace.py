b1fc1ae3ed92395f39c1f5b28037cd8bd479d509
he <abdel.brave@gmail.com>
2023-12-06 15:54:11 -0800

    lS

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for i in range(0, len(my_list)):
+        if my_list[i] == search:
+            my_list[i] = replace
+    return my_list
+
