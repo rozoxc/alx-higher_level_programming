463617453890d374f6ac4914153e5b91c456eb08
he <abdel.brave@gmail.com>
2023-12-06 17:05:54 -0800

    ld

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for i in set_1:
-        for j in set_2:
-            if i == j:
-                return j
+    for element in set_1:
+        common_set = set()
+        if element in set_2:
+            common_set.add(element)
+    return common_set
