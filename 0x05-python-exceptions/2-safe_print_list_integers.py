466a065f5b3c17808273a9af79caff1fd88093d1
he <abdel.brave@gmail.com>
2023-12-18 14:54:40 -0800

    ls

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    a = 0
+    try:
+        for i in range(x):
+            if type(my_list[i]) == int:
+                print("{:d}".format(my_list[i]), end="")
+                a += 1
+        print()
+        return a
+    except (IndexError, TypeError):
+        print()
+        return a
