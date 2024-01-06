4e96ec5ba127da0713d44ccd2cfeb44b8d44bae7
he <abdel.brave@gmail.com>
2024-01-06 15:44:00 -0800

    ls

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -103,7 +103,7 @@ class Rectangle:
         """
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
-    def __del___(self):
+    def __del__(self):
         """del method
         """
         print("Bye rectangle...")
