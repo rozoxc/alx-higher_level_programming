032f83d37ebbb4ce258cff5d5bc3472be16e58a6
he <abdel.brave@gmail.com>
2024-01-09 15:50:46 -0800

    LS

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,12 +3,8 @@
 
 
 def read_file(filename=""):
-    try:
-        """Print the contents of a UTF8 text file to stdout."""
-        with open(filename, 'r') as file:
-            contents = file.read()
-            print(contents)
-    except FileNotFoundError:
-        print(filename + " is not found")
-    except PermissionError:
-        print("you don't have permission to read " + filename)
+    """read filename."""
+    with open(filename, 'r') as file:
+        contents = file.read()
+        print(contents)
+        file.close()
