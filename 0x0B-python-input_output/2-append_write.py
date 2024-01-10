7c241d42e8c2ac1fcb2d74fae48e445e2ac02bcc
he <abdel.brave@gmail.com>
2024-01-09 16:19:39 -0800

    ls

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,5 +5,4 @@
 def append_write(filename="", text=""):
     """append a string to a UTF8 text file"""
     with open(filename, "a", encoding="utf-8") as f:
-        content = f.write(text)
-    return(content)
+        return f.write(text)
