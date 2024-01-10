14924bbbfbd425dabdf5103e73052fdf8100fb33
he <abdel.brave@gmail.com>
2024-01-09 16:18:13 -0800

    ls

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,4 @@
 def write_file(filename="", text=""):
     """Writes a string to a UTF8 text file"""
     with open(filename, "w", encoding="utf-8") as f:
-        content = f.write(text)
-    return(content)
+        return f.write(text)
