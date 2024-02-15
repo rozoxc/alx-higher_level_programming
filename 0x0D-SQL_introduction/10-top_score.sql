e9495129dd1a8ee90762c992a38ba1354d036d31
he <abdel.brave@gmail.com>
2024-02-15 10:39:28 -0800

    ls

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Script to list all records of the table second_table in the database hbtn_0c_0 in your MySQL server
+-- Select all columns from the second_table and order by score
+SELECT id, name FROM second_table ORDER BY score DESC;
+
