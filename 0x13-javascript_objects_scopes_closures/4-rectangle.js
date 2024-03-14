5730442259467143cfb058d803881cbcc9abe5f0
he <abdel.brave@gmail.com>
2024-03-13 19:34:50 -0700

    sl

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let line = '';
+    for (let i = 0; i < this.width; i++) {
+      line += 'x';
+    } for (let j = 0; j < this.height; j++) {
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
