0f241a05e1082ee5f4e29e64d075e0e76a75fc13
he <abdel.brave@gmail.com>
2024-03-13 19:08:13 -0700

    lS

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let line = '';
+    for (let i = 0; i < this.width; i++) {
+      line += 'x';
+    } for (let j = 0; j < this.height; j++) {
+      console.log(line);
+    }
+  }
+}
+module.exports = Rectangle;
