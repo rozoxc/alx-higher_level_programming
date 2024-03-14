309dc7ad7fba24495ceaa08725decc60c0d45ea5
he <abdel.brave@gmail.com>
2024-03-13 20:24:26 -0700

    ls

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
 const args = process.argv[2];
-if (!args)
-{
-	console.log("No argument");
-}else{
-	console.log(args);
+if (!args) {
+  console.log('No argument');
+} else {
+  console.log(args);
 }
-
