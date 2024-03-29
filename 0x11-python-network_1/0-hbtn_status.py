9fe3a6564998c2dab48729967393aef198628e12
he <abdel.brave@gmail.com>
2024-03-28 18:45:05 -0700

    ls

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Python script that fetches https://alx-intranet.hbtn.io/status'''
+import urllib.request
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        utf8_content = body.decode('utf-8')
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", utf8_content)
