6befb7011ae789b49e12c11f018ab5088b3c6607
he <abdel.brave@gmail.com>
2024-03-28 19:03:44 -0700

    ls

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,7 +4,5 @@
 import urllib.request
 import sys
 if __name__ == "__main__":
-    url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
-        x_request_id = response.headers.get('X-Request-Id')
-        print(x_request_id) 
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers.get('X-Request-Id'))
