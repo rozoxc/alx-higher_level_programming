7c48858040e1615a08e3dda5ff779ecb4000c08b
asa <abde@gmail.com>
2024-05-02 22:52:14 +0000

    ls

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});document.addEventListener('DOMContentLoaded', function () {
+  var header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
